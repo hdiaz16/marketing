b77e05a117dda1ea08ad4183510ab5b8f936d2c1
hector diaz <hfdb16@hotmail.com>
2018-10-03 10:45:47 -0500

    agenda

--- assets/js/dataTables.js
+++ assets/js/dataTables.js
@@ -1,18 +1,18 @@
- // SideNav Initialization
-        $(".button-collapse").sideNav();
+// SideNav Initialization
+$(".button-collapse").sideNav();
 
-        var container = document.querySelector('.custom-scrollbar');
-        Ps.initialize(container, {
-            wheelSpeed: 2,
-            wheelPropagation: true,
-            minScrollbarLength: 20
-        });
+var container = document.querySelector('.custom-scrollbar');
+Ps.initialize(container, {
+    wheelSpeed: 2,
+    wheelPropagation: true,
+    minScrollbarLength: 20
+});
 
-        $(document).ready(function() {
-            $('#datatables').DataTable();
-        });
+$(document).ready(function() {
+    $('#datatables').DataTable();
+});
 
-        // Material Select Initialization
-        $(document).ready(function () {
-            $('select[name="datatables_length"]').material_select();
-        });
+// Material Select Initialization
+$(document).ready(function () {
+    $('select[name="datatables_length"]').material_select();
+});
